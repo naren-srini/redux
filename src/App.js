63d1626653781585cf25017d7041b02f6eb7d080
Naren <narayanan@welcometocbs.com>
2021-06-01 20:06:35 -0400

    Fixed the linting issues that caused the code to break

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App"> 
-    <h2> hello World </h2>
+    return ( 
+    <div className='App'>
+        <h2>Hello World, this is React on Redux</h2>
     </div>
-  );
+);
 }
 
 export default App;
